20b494709aee4699a457f85494b7785e34bc47c2
JimH <Jim.Halewijn@tweesnoeken.nl>
2021-02-09 13:56:00 +0100

    fix for incorrect asset path and cap

--- src/app/components/qubusSimple/qubusSimple.component.ts
+++ src/app/components/qubusSimple/qubusSimple.component.ts
@@ -49,7 +49,7 @@ export class QubusSimpleComponent implements OnInit {
 
     // load a sound and set it as the Audio object's buffer
     const audioLoader = new THREE.AudioLoader();
-    audioLoader.load( '../../../assets/sounds/streets.mp3', function( buffer ) {
+    audioLoader.load( 'assets/sounds/Streets.mp3', function( buffer ) {
       sound.setBuffer( buffer );
       sound.setLoop( true );
       sound.setVolume( 0.5 );
