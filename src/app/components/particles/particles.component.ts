3ebbb03b0b2e2d15884acfd7dee48c623a370b96
RubenR <ruben.van.rijen@tweesnoeken.nl>
2021-02-09 10:15:45 +0100

    beter gemaakt

--- src/app/components/particles/particles.component.ts
+++ src/app/components/particles/particles.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RendererService } from 'src/app/services/renderer/renderer.service';
 import * as THREE from 'three';
 
 @Component({
@@ -8,85 +9,65 @@ import * as THREE from 'three';
 })
 export class ParticlesComponent implements OnInit {
   camera;
-  scene; 
-  renderer; 
+  scene;
+  renderer;
   stats;
   material;
   mouseX = 0;
   mouseY = 0;
+  canvas: HTMLCanvasElement;
 
-	windowHalfX = window.innerWidth / 2;
-	windowHalfY = window.innerHeight / 2;
 
-  constructor() { }
+  windowHalfX = window.innerWidth / 2;
+  windowHalfY = window.innerHeight / 2;
+
+  constructor(private rendererService: RendererService) { }
 
   ngOnInit(): void {
     this.init();
-		this.animate();
+    this.animate();
   }
 
   init() {
-    this.camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 2, 2000 );
+    this.camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 2, 2000);
     this.camera.position.z = 1000;
 
     this.scene = new THREE.Scene();
-    this.scene.fog = new THREE.FogExp2( 0x000000, 0.001 );
+    this.scene.fog = new THREE.FogExp2(0x000000, 0.001);
 
     const geometry = new THREE.BufferGeometry();
     const vertices = [];
 
-    const sprite = new THREE.TextureLoader().load( '../../../assets/sprites/disc.png' );
+    const sprite = new THREE.TextureLoader().load('../../../assets/sprites/disc.png');
 
-    for ( let i = 0; i < 10000; i ++ ) {
+    for (let i = 0; i < 10000; i++) {
 
       const x = 2000 * Math.random() - 1000;
       const y = 2000 * Math.random() - 1000;
       const z = 2000 * Math.random() - 1000;
 
-      vertices.push( x, y, z );
+      vertices.push(x, y, z);
 
     }
 
-    geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
 
-    this.material = new THREE.PointsMaterial( { size: 35, sizeAttenuation: true, map: sprite, alphaTest: 0.5, transparent: true } );
-    this.material.color.setHSL( 1.0, 0.3, 0.7 );
+    this.material = new THREE.PointsMaterial({ size: 35, sizeAttenuation: true, map: sprite, alphaTest: 0.5, transparent: true });
+    this.material.color.setHSL(1.0, 0.3, 0.7);
 
-    const particles = new THREE.Points( geometry, this.material );
-    this.scene.add( particles );
+    const particles = new THREE.Points(geometry, this.material);
+    this.scene.add(particles);
 
-    //
 
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setPixelRatio( window.devicePixelRatio );
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( this.renderer.domElement );
+    this.canvas = document.querySelector("#C");
 
-    //
-
-    //this.stats = new Stats();
-    //document.body.appendChild( stats.dom );
-
-    //
-
-    //const gui = new GUI();
-
-    //gui.add( material, 'sizeAttenuation' ).onChange( function () {
-
-      //material.needsUpdate = true;
-
-    //} );
-
-    //gui.open();
-
-    //
-
+    this.renderer = this.rendererService.createRender('#c',window.innerWidth, window.innerHeight );
+    this.renderer.setPixelRatio(window.devicePixelRatio);
     document.body.style.touchAction = 'none';
-    document.body.addEventListener( 'pointermove', this.onPointerMove );
+    document.body.addEventListener('pointermove', () => this.onPointerMove(event));
 
-    //
 
-    window.addEventListener( 'resize', this.onWindowResize );
+    window.addEventListener('resize', this.onWindowResize);
 
   }
 
@@ -94,18 +75,14 @@ export class ParticlesComponent implements OnInit {
 
     this.windowHalfX = window.innerWidth / 2;
     this.windowHalfY = window.innerHeight / 2;
-
     this.camera.aspect = window.innerWidth / window.innerHeight;
     this.camera.updateProjectionMatrix();
-
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
 
   }
 
-  onPointerMove( event ) {
-
-    if ( event.isPrimary === false ) return;
-
+  onPointerMove(event) {
+    if (event.isPrimary === false) return;
     this.mouseX = event.clientX - this.windowHalfX;
     this.mouseY = event.clientY - this.windowHalfY;
 
@@ -114,9 +91,7 @@ export class ParticlesComponent implements OnInit {
   //
 
   animate() {
-
-    requestAnimationFrame( () => this.animate() );
-
+    requestAnimationFrame(() => this.animate());
     this.render();
     //stats.update();
   }
@@ -125,15 +100,15 @@ export class ParticlesComponent implements OnInit {
 
     const time = Date.now() * 0.00005;
 
-    this.camera.position.x += ( this.mouseX - this.camera.position.x ) * 0.05;
-    this.camera.position.y += ( - this.mouseY - this.camera.position.y ) * 0.05;
+    this.camera.position.x += (this.mouseX - this.camera.position.x) * 0.05;
+    this.camera.position.y += (- this.mouseY - this.camera.position.y) * 0.05;
 
-    this.camera.lookAt( this.scene.position );
+    this.camera.lookAt(this.scene.position);
 
-    const h = ( 360 * ( 1.0 + time ) % 360 ) / 360;
-    this.material.color.setHSL( h, 0.5, 0.5 );
+    const h = (360 * (1.0 + time) % 360) / 360;
+    this.material.color.setHSL(h, 0.5, 0.5);
 
-    this.renderer.render( this.scene, this.camera );
+    this.renderer.render(this.scene, this.camera);
 
   }
 }
