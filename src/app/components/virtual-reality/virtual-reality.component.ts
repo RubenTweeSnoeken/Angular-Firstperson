ef84a09c7309cbbc027606204623fcdc1cd288df
JimH <Jim.Halewijn@tweesnoeken.nl>
2021-02-09 13:57:56 +0100

    audio for vr poc

--- src/app/components/virtual-reality/virtual-reality.component.ts
+++ src/app/components/virtual-reality/virtual-reality.component.ts
@@ -126,6 +126,23 @@ export class VirtualRealityComponent implements OnInit {
     //
 
     window.addEventListener( 'resize', this.onWindowResize );
+
+    // Audio
+    // create an AudioListener and add it to the camera
+    const listener = new THREE.AudioListener();
+    this.camera.add( listener );
+
+    // create a global audio source
+    const sound = new THREE.Audio( listener );
+
+    // load a sound and set it as the Audio object's buffer
+    const audioLoader = new THREE.AudioLoader();
+    audioLoader.load( 'assets/sounds/Streets.mp3', function( buffer ) {
+      sound.setBuffer( buffer );
+      sound.setLoop( true );
+      sound.setVolume( 0.5 );
+      sound.play();
+    });
   }
 
   onWindowResize() {
