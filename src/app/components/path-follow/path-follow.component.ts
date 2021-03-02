846a90359fa7da5cb3423e430b5cdcf47433a36b
rgjrijen <ybK62Hm&>
2021-03-02 10:45:18 +0100

    Better code and gui deleted

--- src/app/components/path-follow/path-follow.component.ts
+++ src/app/components/path-follow/path-follow.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import {GUI} from 'three/examples/jsm/libs/dat.gui.module.js';
 import * as THREE from 'three';
 import {Curves} from 'three/examples/jsm/curves/CurveExtras.js';
 
@@ -18,13 +17,11 @@ export class PathFollowComponent implements OnInit {
   splineCamera: THREE.PerspectiveCamera;
   cameraHelper: THREE.CameraHelper;
   cameraEye: THREE.Mesh;
-
   direction: THREE.Vector3;
   binormal: THREE.Vector3;
   normal: THREE.Vector3;
   position: THREE.Vector3;
   lookAt: THREE.Vector3;
-
   pipeSpline: THREE.CatmullRomCurve3;
   sampleClosedSpline: THREE.CatmullRomCurve3;
   splines: any;
@@ -57,7 +54,6 @@ export class PathFollowComponent implements OnInit {
       new THREE.Vector3(70, 0, 0), new THREE.Vector3(80, 0, 0),
       new THREE.Vector3(90, 0, 0), new THREE.Vector3(100, 0, 0)
     ]);
-
     this.sampleClosedSpline = new THREE.CatmullRomCurve3([
       new THREE.Vector3(0, -40, -40),
       new THREE.Vector3(0, 40, -40),
@@ -69,40 +65,20 @@ export class PathFollowComponent implements OnInit {
     this.sampleClosedSpline.curveType = 'catmullrom';
     // @ts-ignore
     this.sampleClosedSpline.closed = true;
-
     this.splines = {
-      GrannyKnot: new Curves.GrannyKnot(),
-      HeartCurve: new Curves.HeartCurve(3.5),
-      VivianiCurve: new Curves.VivianiCurve(70),
-      KnotCurve: new Curves.KnotCurve(),
-      HelixCurve: new Curves.HelixCurve(),
-      TrefoilKnot: new Curves.TrefoilKnot(),
-      TorusKnot: new Curves.TorusKnot(20),
-      CinquefoilKnot: new Curves.CinquefoilKnot(20),
-      TrefoilPolynomialKnot: new Curves.TrefoilPolynomialKnot(14),
-      FigureEightPolynomialKnot: new Curves.FigureEightPolynomialKnot(),
-      DecoratedTorusKnot4a: new Curves.DecoratedTorusKnot4a(),
-      DecoratedTorusKnot4b: new Curves.DecoratedTorusKnot4b(),
-      DecoratedTorusKnot5a: new Curves.DecoratedTorusKnot5a(),
       DecoratedTorusKnot5c: new Curves.DecoratedTorusKnot5c(),
-      PipeSpline: this.pipeSpline,
-      SampleClosedSpline: this.sampleClosedSpline
     };
-
-
     this.params = {
-      spline: 'GrannyKnot',
+      spline: 'DecoratedTorusKnot5c',
       scale: 4,
-      extrusionSegments: 100,
+      extrusionSegments: 500,
       radiusSegments: 3,
       closed: true,
       animationView: false,
       lookAhead: false,
       cameraHelper: false,
     };
-
     this.material = new THREE.MeshLambertMaterial({color: 0xFF0000});
-
     this.wireframeMaterial = new THREE.MeshBasicMaterial({
       color: 0xFFFFFF,
       opacity: 0.3,
@@ -112,7 +88,7 @@ export class PathFollowComponent implements OnInit {
     this.renderer = new THREE.WebGLRenderer({antialias: true});
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.init();
     this.keyStrokes();
     this.animate();
@@ -122,11 +98,11 @@ export class PathFollowComponent implements OnInit {
     const onKeyDown = (event) => {
       switch (event.code) {
         case 'KeyP':
-          this.params.animationView = true;
+          this.params.animationView = !this.params.animationView;
           this.animateCamera();
           break;
         case 'KeyZ':
-          this.params.animationView = false;
+          this.params.cameraHelper = !this.params.cameraHelper;
           this.animateCamera();
           break;
       }
@@ -140,7 +116,12 @@ export class PathFollowComponent implements OnInit {
       this.mesh.geometry.dispose();
     }
     const extrudePath = this.splines[this.params.spline];
-    this.tubeGeometry = new THREE.TubeGeometry(extrudePath, this.params.extrusionSegments, 2, this.params.radiusSegments, this.params.closed);
+    this.tubeGeometry = new THREE.TubeGeometry(
+      extrudePath,
+      this.params.extrusionSegments,
+      2,
+      this.params.radiusSegments,
+      this.params.closed);
     this.addGeometry(this.tubeGeometry);
     this.setScale();
   }
@@ -166,104 +147,37 @@ export class PathFollowComponent implements OnInit {
   init() {
     this.container = document.getElementById('container');
     // camera
-
     this.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 10000);
     this.camera.position.set(0, 50, 500);
-
     // scene
-
     this.scene = new THREE.Scene();
     this.scene.background = new THREE.Color(0xf0f0f0);
-
     // light
-
     const light = new THREE.DirectionalLight(0xffffff);
     light.position.set(0, 0, 1);
     this.scene.add(light);
-
     // tube
-
     this.parent = new THREE.Object3D();
     this.scene.add(this.parent);
-
     this.splineCamera = new THREE.PerspectiveCamera(84, window.innerWidth / window.innerHeight, 0.01, 1000);
     this.parent.add(this.splineCamera);
-
     this.cameraHelper = new THREE.CameraHelper(this.splineCamera);
     this.scene.add(this.cameraHelper);
-
     this.addTube();
-
     // debug camera
-
     this.cameraEye = new THREE.Mesh(new THREE.SphereGeometry(5), new THREE.MeshBasicMaterial({color: 0xFFFFFF}));
     this.parent.add(this.cameraEye);
-
     this.cameraHelper.visible = this.params.cameraHelper;
     this.cameraEye.visible = this.params.cameraHelper;
-
     // renderer
-
     this.renderer = new THREE.WebGLRenderer({antialias: true});
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.container.appendChild(this.renderer.domElement);
-
-    // stats
-
-
-    // dat.GUI
-
-    const gui = new GUI({width: 300});
-
-    const folderGeometry = gui.addFolder('Geometry');
-    folderGeometry.add(this.params, 'spline', Object.keys(this.splines)).onChange(() => {
-      this.addTube();
-    });
-    folderGeometry.add(this.params, 'scale', 2, 10).step(2).onChange(() => {
-
-      this.setScale();
-
-    });
-    folderGeometry.add(this.params, 'extrusionSegments', 50, 500).step(50).onChange(() => {
-
-      this.addTube();
-
-    });
-    folderGeometry.add(this.params, 'radiusSegments', 2, 12).step(1).onChange(() => {
-
-      this.addTube();
-
-    });
-    folderGeometry.add(this.params, 'closed').onChange(() => {
-
-      this.addTube();
-
-    });
-    folderGeometry.open();
-
-    const folderCamera = gui.addFolder('Camera');
-    folderCamera.add(this.params, 'animationView').onChange(() => {
-
-      this.animateCamera();
-
-    });
-    folderCamera.add(this.params, 'lookAhead').onChange(() => {
-
-      this.animateCamera();
-
-    });
-    folderCamera.add(this.params, 'cameraHelper').onChange(() => {
-
-      this.animateCamera();
-
-    });
-    folderCamera.open();
-
     const controls = new OrbitControls(this.camera, this.renderer.domElement);
     controls.minDistance = 100;
     controls.maxDistance = 2000;
-    window.addEventListener('resize', this.onWindowResize);
+    window.addEventListener('resize', () => this.onWindowResize());
   }
 
   onWindowResize() {
@@ -284,43 +198,29 @@ export class PathFollowComponent implements OnInit {
 
   render() {
     // animate camera along spline
-
     const time = Date.now();
-    const looptime = 20 * 1000;
-    const t = (time % looptime) / looptime;
-
+    const loopTime = 100 * 1000;
+    const t = (time % loopTime) / loopTime;
     this.tubeGeometry.parameters.path.getPointAt(t, this.position);
     this.position.multiplyScalar(this.params.scale);
-
     // interpolation
-
     const segments = this.tubeGeometry.tangents.length;
     const pickt = t * segments;
     const pick = Math.floor(pickt);
     const pickNext = (pick + 1) % segments;
-
     this.binormal.subVectors(this.tubeGeometry.binormals[pickNext], this.tubeGeometry.binormals[pick]);
     this.binormal.multiplyScalar(pickt - pick).add(this.tubeGeometry.binormals[pick]);
-
     this.tubeGeometry.parameters.path.getTangentAt(t, this.direction);
     const offset = 15;
-
     this.normal.copy(this.binormal).cross(this.direction);
-
     // we move on a offset on its binormal
-
     this.position.add(this.normal.clone().multiplyScalar(offset));
-
     this.splineCamera.position.copy(this.position);
     this.cameraEye.position.copy(this.position);
-
     // using arclength for stablization in look ahead
-
     this.tubeGeometry.parameters.path.getPointAt((t + 30 / this.tubeGeometry.parameters.path.getLength()) % 1, this.lookAt);
     this.lookAt.multiplyScalar(this.params.scale);
-
     // camera orientation 2 - up orientation via normal
-
     if (!this.params.lookAhead) {
       this.lookAt.copy(this.position).add(this.direction);
     }
@@ -328,7 +228,5 @@ export class PathFollowComponent implements OnInit {
     this.splineCamera.quaternion.setFromRotationMatrix(this.splineCamera.matrix);
     this.cameraHelper.update();
     this.renderer.render(this.scene, this.params.animationView === true ? this.splineCamera : this.camera);
-
   }
-
 }
